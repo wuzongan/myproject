fdd917efa139a7693fdd7e5821427eb3cd7a1033
wuzongan <wuzongan349@sina.com>
2015-12-25 11:29:41 +0800

    no message

--- project2/frameworks/runtime-src/proj.android_no_anysdk/obj/local/armeabi/objs-debug/cocos2dx_internal_static/base/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-./obj/local/armeabi/objs-debug/cocos2dx_internal_static/base/ccFPSImages.o: \
-  /Users/tang/Desktop/turnbasedbattle/quick-3.6/cocos/./base/ccFPSImages.c \
-  /Users/tang/Desktop/turnbasedbattle/quick-3.6/cocos/./base/ccFPSImages.h
-
-/Users/tang/Desktop/turnbasedbattle/quick-3.6/cocos/./base/ccFPSImages.h:
